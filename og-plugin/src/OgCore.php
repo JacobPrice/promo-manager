bf6358d5c7c927d83d0eb1ddaf9012b974f0533e
Jacob Price <J@cobprice.com>
2023-10-04 21:57:42 -0400

    Create method runner for ogcore

--- og-plugin/src/OgCore.php
+++ og-plugin/src/OgCore.php
@@ -2,40 +2,68 @@
 
 namespace OgPlugin;
 
-
-/**
- * OgCore
- * 
- * This class is a wrapper for WordPress add_action and add_filter functions.
- */
 abstract class OgCore {
-public function __construct(
-        protected $actions = [],
-        protected $filters = [],
-    ) {
+
+    protected $actions = [];
+    protected $filters = [];
+    private $instances = [];
+    private $hook_info = [];
+
+    public function __construct() {
         $this->init();
         $this->add_hooks();
     }
-    // TODO: Move init and on_admin to interface contract.
-    public function init() {}
-    public function on_admin() {}
+
+    protected function init() {}
+    protected function on_admin() {}
+
     public function add_action($hook, $callback, $priority = 10, $accepted_args = 1) {
-        $this->actions[] = compact('hook', 'callback', 'priority', 'accepted_args');
-        return $this;
+        add_action($hook, [$this, 'method_runner'], $priority, $accepted_args);
+        $this->store_hook($hook, $callback, $accepted_args);
     }
 
     public function add_filter($hook, $callback, $priority = 10, $accepted_args = 1) {
-        $this->filters[] = compact('hook', 'callback', 'priority', 'accepted_args');
-        return $this;
+        add_filter($hook, [$this, 'method_runner'], $priority, $accepted_args);
+        $this->store_hook($hook, $callback, $accepted_args);
+    }
+
+    private function store_hook($hook, $callback, $accepted_args) {
+        $this->hook_info[$hook][] = [
+            'class' => $callback[0],
+            'method' => $callback[1],
+            'args_count' => $accepted_args
+        ];
+    }
+
+    public function method_runner() {
+        $hook = current_filter();
+        if (isset($this->hook_info[$hook])) {
+            $args = func_get_args();
+            foreach ($this->hook_info[$hook] as $info) {
+                if (count($args) > $info['args_count']) {
+                    $args = array_slice($args, 0, $info['args_count']);
+                }
+                $this->runMethod($info['class'], $info['method'], $args);
+            }
+        }
+    }
+
+    private function runMethod($class_name, $method_name, array $args = []) {
+        if (!isset($this->instances[$class_name])) {
+            $this->instances[$class_name] = new $class_name();
+        }
+
+        call_user_func_array([$this->instances[$class_name], $method_name], $args);
     }
 
     public function add_hooks() {
         foreach ($this->actions as $action) {
-            add_action($action['hook'], $action['callback'], $action['priority'], $action['accepted_args']);
+            $this->add_action($action['hook'], $action['callback'], $action['priority'], $action['accepted_args']);
         }
 
         foreach ($this->filters as $filter) {
-            add_filter($filter['hook'], $filter['callback'], $filter['priority'], $filter['accepted_args']);
+            $this->add_filter($filter['hook'], $filter['callback'], $filter['priority'], $filter['accepted_args']);
         }
     }
+
 }
